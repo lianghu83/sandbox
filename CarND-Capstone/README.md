35654955577ee05c594351f64f43887254b4386e
Vandan Juvekar <onedanj@gmail.com>
2019-02-12 22:17:05 -0500

    Use OpenCV DNN API instead of keras-yolo3 conversion for traffic light detection

--- CarND-Capstone/README.md
+++ CarND-Capstone/README.md
@@ -4,8 +4,8 @@ This is the project repo for the final project of the Udacity Self-Driving Car N
 
 We used the following approach for Traffic Light Detection:
 1. Download and install [Darknet](https://pjreddie.com/darknet/yolo/).
-2. Download pre-trained YOLOv3 tiny model [weights](https://pjreddie.com/media/files/yolov3-tiny.weights) and [config](https://github.com/pjreddie/darknet/blob/master/cfg/yolov3-tiny.cfg).
-3. YOLO detects 80 classes by default. Follow instructions [here](https://github.com/AlexeyAB/darknet#how-to-train-to-detect-your-custom-objects) to edit config file, create classes file (traffic_lights-obj.names), metadata file (traffic_lights.data) to adapt them to our need of detecting 3 classes (red, yello, green lights).
+2. Download pre-trained YOLOv2 tiny model [weights](https://pjreddie.com/media/files/yolov2-tiny.weights) and [config](https://github.com/pjreddie/darknet/blob/master/cfg/yolov2-tiny.cfg). (Since YOLOv3 is not compatible with OpenCV 3.3.1 used in ROS Kinetic, we chose to use YOLOv2.)
+3. YOLO detects 80 classes by default. Follow instructions [here](https://github.com/AlexeyAB/darknet/tree/47c7af1cea5bbdedf1184963355e6418cb8b1b4f#how-to-train-tiny-yolo-to-detect-your-custom-objects) to edit config file, create classes file (traffic_lights-obj.names), metadata file (traffic_lights.data) to adapt them to our need of detecting 3 classes (red, yello, green lights).
 4. Collect training image data from simulator and actual test track by installing and using ROS package `image_view`, as follows:
 
 `apt-get install -y ros-kinetic-image-view`
@@ -19,21 +19,9 @@ and
 `rosrun image_view image_saver image:=/image_raw _filename_format:="tlt%04i.%s" _sec_per_frame:=0.2`
 
 5. Select training images - roughly equal number of red, yellow, green, and unknown/no light scenarios, and label them using a labeling tool. We used [labelImg](https://github.com/tzutalin/labelImg).
-6. Follow the instructions [here](https://github.com/AlexeyAB/darknet#how-to-train-tiny-yolo-to-detect-your-custom-objects) to start training a classifier for traffic light detection.
-7. Stop training when the average loss (error) is less than 0.5. Save the weights file, e.g. `traffic_lights-simulator.weights`
-8. Convert the resulting YOLO v3 weights to Keras HDF5 format using a YOLO v3 Keras conversion and implementation tool. We used [keras-yolo3](https://github.com/qqwweee/keras-yolo3), as follows:
-
-`pip install configparser`
-
-`cd /capstone/ros/src/tl_detector/light_classification/yolo3/train`
-
-e.g. converting YOLOv3 weights for simulator images to Keras HDF5 format:
-
-`python convert.py traffic_lights-tiny.cfg ../traffic_lights-simulator.weights ../traffic_lights_weights-simulator.h5`
-
-`rm ../traffic_lights-simulator.weights`
-
-9. Update tl_classifier.py to use the weights along with the keras-yolo3 API to predict traffic lights and their states. Ensure appropriate weights file is used for simulator vs. site case.
+6. Follow the instructions [here](https://github.com/AlexeyAB/darknet/tree/47c7af1cea5bbdedf1184963355e6418cb8b1b4f#how-to-train-tiny-yolo-to-detect-your-custom-objects) to start training a classifier for traffic light detection.
+7. Stop training when the average loss (error) is less than 0.5.
+8. Update tl_classifier.py to use the weights along with OpenCV API to predict traffic lights with states.. Ensure appropriate weights file is used for simulator vs. site case.
 
 ## Installation
 
--- CarND-Capstone/ros/src/tl_detector/light_classification/tl_classifier.py
+++ CarND-Capstone/ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,61 +1,33 @@
 from __future__ import division
 
-import rospy
 import cv2
 import numpy as np
-import glob
-import tensorflow as tf
-from PIL import Image
-from keras import backend as K
-from keras.models import load_model
-from keras.layers import Input
 from styx_msgs.msg import TrafficLight
-
-from yolo3.model import yolo_eval
 
 class TLClassifier(object):
     def __init__(self, simulator_mode = True):
         # Initialize YOLO detection parameters
-        self.detection_threshold = 0.35
-        self.iou_threshold = 0.45
+        self.detection_threshold = 0.7
         self.in_width = 416
         self.in_height = 416
         self.scale = 1/255
         self.num_classes = 3 # Classes: RED = 0, YELLOW = 1, GREEN = 2
 
-        model_path = '/capstone/ros/src/tl_detector/light_classification/yolo3/traffic_lights_weights-{}.h5'
+        model_config = '/capstone/ros/src/tl_detector/light_classification/traffic_lights-yolov2-tiny.cfg'
+        model_weights = '/capstone/ros/src/tl_detector/light_classification/traffic_lights_model-{}.weights'
         if simulator_mode:
-            model_path = model_path.format('simulator')
+            model_weights = model_weights.format('simulator')
         else:
-            model_path = model_path.format('site')
+            model_weights = model_weights.format('site')
 
-        # rospy.loginfo("Loading %s", model_path)
-        # YOLOv3-tiny anchors
-        anchors = [10.,14.,  23.,27.,  37.,58.,  81.,82.,  135.,169.,  344.,319.]
-        self.anchors = np.array(anchors).reshape(-1, 2)
+        self.net = cv2.dnn.readNetFromDarknet(model_config, model_weights)
 
-        self.sess = K.get_session()
-        self.model = load_model(model_path, compile=False)
-        self.input_image_shape = K.placeholder(shape=(2, ))
-
-        self.boxes, self.scores, self.classes = yolo_eval(self.model.output,
-                        self.anchors, self.num_classes, self.input_image_shape,
-                        score_threshold=self.detection_threshold,
-                        iou_threshold=self.iou_threshold)
-        self.graph = tf.get_default_graph()
-
-    def letterbox_image(self, image, size):
-        '''resize image with unchanged aspect ratio using padding'''
-        iw, ih = image.size
-        w, h = size
-        scale = min(w/iw, h/ih)
-        nw = int(iw*scale)
-        nh = int(ih*scale)
-
-        image = image.resize((nw,nh), Image.BICUBIC)
-        new_image = Image.new('RGB', size, (128,128,128))
-        new_image.paste(image, ((w-nw)//2, (h-nh)//2))
-        return new_image
+    # Get the names of the output layers
+    def get_output_layers(self):
+        # Get the names of all the layers in the network
+        layersNames = self.net.getLayerNames()
+        # Get the names of the output layers, i.e. the layers with unconnected outputs
+        return [layersNames[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -67,22 +39,24 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        # Process image to prepare input
-        image = Image.fromarray(image)
-        boxed_image = self.letterbox_image(image, (self.in_width, self.in_height))
-        image_data = np.array(boxed_image, dtype='float32')
-        image_data *= self.scale
-        # Add batch dimension
-        image_data = np.expand_dims(image_data, 0)
+        # Create input blob and send it to the network
+        blob = cv2.dnn.blobFromImage(image, self.scale,
+            (self.in_width, self.in_height), (0,0,0), True, crop=False)
+        self.net.setInput(blob)
 
-        with self.graph.as_default():
-            out_boxes, out_scores, out_classes = self.sess.run(
-                [self.boxes, self.scores, self.classes],
-                feed_dict={
-                    self.model.input: image_data,
-                    self.input_image_shape: [image.size[1], image.size[0]],
-                    K.learning_phase(): 0
-                })
+        # Run inference through the network
+        outs = self.net.forward(self.get_output_layers())
+
+        # Gather predictions from output layers
+        out_classes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                classId = np.argmax(scores)
+                confidence = scores[classId]
+                # Filter out predictions below threshold
+                if confidence > self.detection_threshold:
+                    out_classes.append(classId)
 
         state = TrafficLight.UNKNOWN
         # In case of multiple traffic lights are detected in an image,
